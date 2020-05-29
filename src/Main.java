897523c33bb04175c978b9748a1d007c92a9365e
erayayaz <eray19ayaz@gmail.com>
2020-05-29 20:25:00 +0300

    added arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+    ArrayList<Integer> cityID = new ArrayList<Integer>();
+    ArrayList<Integer> cityX = new ArrayList<Integer>();
+    ArrayList<Integer> cityY = new ArrayList<Integer>();
     public static void main(String[] args) throws FileNotFoundException {
 	    reading();
     }
     public static void reading() throws FileNotFoundException {
         Scanner sc = new Scanner(new File("input.txt"));
-        Cities c = new Cities(5,4,3);
         
         while (sc.hasNextLine()) {
             Scanner src = new Scanner(sc.nextLine());
             while (src.hasNext()) {
                 if (src.hasNextInt()) {
-                    System.out.println("a :"+src.nextInt());
-                    System.out.println("b :"+src.nextInt());
-                    System.out.println("c :"+src.nextInt());
+                    int a=src.nextInt();
+                    int b=src.nextInt();
+                    int c=src.nextInt();
+
                 } else
                     src.next();
 
