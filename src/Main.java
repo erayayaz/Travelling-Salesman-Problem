e049b55edfac2c151a23b64b7ef3ff9e35ce5f88
erayayaz <eray19ayaz@gmail.com>
2020-06-11 00:45:26 +0300

    2-opt

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,49 @@ import java.util.Scanner;
 
 public class Main {
     static ArrayList<City> route = new ArrayList<>();
+    static ArrayList<City> newRoute = new ArrayList<>();
     static ArrayList<City> cities = new ArrayList<>();
+    static int counter=0;
+    static double best=0;
+
+    public static void chosenPoint(){
+
+        for(int i=1;i<route.size()-2;i++){
+            for(int j=i+1;j<route.size()-1;j++){
 
 
+                double distance1=(Math.pow(route.get(i).getX()-route.get(i-1).getX(), 2) + Math.pow(route.get(i).getY() - route.get(i-1).getY(), 2))+
+                        (Math.pow(route.get(j).getX()-route.get(j+1).getX(), 2) + Math.pow(route.get(j).getY() - route.get(j+1).getY(), 2));
+
+                double distance2=(Math.pow(route.get(j).getX()-route.get(i-1).getX(), 2) + Math.pow(route.get(j).getY() - route.get(i-1).getY(), 2))+
+                        (Math.pow(route.get(i).getX()-route.get(j+1).getX(), 2) + Math.pow(route.get(i).getY() - route.get(j+1).getY(), 2));
+
+                newRoute =opt2Swap(route,i,j);
+                double newBest =findTotalDistance(newRoute);
+
+                if(newBest<best){
+                    route= newRoute;
+                    best=newBest;
+                    counter++;
+                }
+            }
+        }
+    }
+
+    public static ArrayList<City> opt2Swap(ArrayList<City> routes, int i, int j) {
+        ArrayList<City> newPath = new ArrayList<>();
+        for(int x=0;x<i;x++){
+            newPath.add(routes.get(x));
+        }
+        for(int y=j;y>=i;y--){
+            newPath.add(routes.get(y));
+        }
+
+        for(int z=j+1;z< route.size();z++){
+            newPath.add(route.get(z));
+        }
+        return newPath;
+    }
     public static int findNeighbour(City c) {
         int cX = c.getX();
         int cY = c.getY();
@@ -31,7 +71,14 @@ public class Main {
         }
         return neighbourIndex;
     }
-
+    public static double findTotalDistance(ArrayList<City> route){
+        double sum=0;
+        for(int i = 0; i<route.size()-1;i++){
+            double distance =  Math.sqrt(Math.pow(route.get(i).getX()-route.get(i+1).getX(), 2) + Math.pow(route.get(i).getY() - route.get(i+1).getY(), 2));
+            sum +=distance;
+        }
+        return sum;
+    }
     public static void createRoute() {
         route.add(cities.get(0));
         cities.get(0).setVisited(1);
@@ -45,7 +92,7 @@ public class Main {
     }
 
     public static void reading() throws FileNotFoundException {
-        Scanner sc = new Scanner(new File("/Users/morsi/IdeaProjects/Travelling-Salesman-Problem/input.txt"));
+        Scanner sc = new Scanner(new File("input.txt"));
 
         while (sc.hasNextLine()) {
             Scanner src = new Scanner(sc.nextLine());
@@ -67,5 +114,25 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         reading();
         createRoute();
+        for(int i=0;i<route.size();i++){
+            System.out.print(route.get(i).getId() + "  ");
+        }
+        /*length with nearest neigbout algorithm*/
+        best=findTotalDistance(route);
+        System.out.println();
+        System.out.println("Length is " + findTotalDistance(route));
+
+
+        /*Trying 2-opt algorithm*/
+        chosenPoint();
+        for(int i=0;i<route.size();i++){
+            System.out.print(route.get(i).getId() + "  ");
+        }
+        /*length with 2-opt algorithm*/
+        double last = findTotalDistance(route);
+        System.out.println();
+        System.out.println("Length is " + findTotalDistance(route));
+        System.out.println(counter);
+
     }
 }
